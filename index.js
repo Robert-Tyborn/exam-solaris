347079b3fe9944820837a07186fed5bd3ea2f5af
Robert-Tyborn <144322797+Robert-Tyborn@users.noreply.github.com>
2023-11-10 15:39:30 +0100

    ...

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const BASE_URL =  'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/'
+const BASE_URL = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/'
+
+let bodies = [];
 
 async function getSolarisBodies() {
-    let resp = await fetch(`${BASE_URL}/bodies`, {
-        method: "GET",
-        headers: {"x-zocom": "solaris-1Cqgm3S6nlMechWO"},
-    });
-    const data = await resp.json()
-}
-const data = getSolarisBodies()
+  let resp = await fetch(`${BASE_URL}/bodies`, {
+    method: "GET",
+    headers: { "x-zocom": "solaris-1Cqgm3S6nlMechWO" },
+  });
 
-console.log(data)
+  const data = await resp.json()
+  bodies = data.bodies;
+
+}
+getSolarisBodies().then(() => {
+    console.log(bodies)
+});
+
+
+
+
+
+
